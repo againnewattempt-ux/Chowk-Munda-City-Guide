fee280c18e216ccfa46db6a9cbc1854499f8198f
againnewattempt-ux <againnewattempt@gmail.com>
2026-07-19 19:48:25 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+const searchInput = document.getElementById("searchInput");
 
+searchInput.addEventListener("keyup", function () {
+  const value = this.value.toLowerCase();
+
+  const shops = document.querySelectorAll(".shop-card");
+
+  shops.forEach(shop => {
+    const text = shop.innerText.toLowerCase();
+
+    if (text.includes(value)) {
+      shop.style.display = "flex";
+    } else {
+      shop.style.display = "none";
+    }
+  });
+});
